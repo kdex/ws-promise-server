073264c7a867821a703abba0b7a8434045fa6edd
kdex <kdex@kdex.de>
2016-07-02 01:54:09 +0200

    Fixed proper event forwarding

--- src/Server.js
+++ src/Server.js
@@ -1,4 +1,7 @@
-import EventEmitter from "crystal-event-emitter";
+import {
+	default as EventEmitter,
+	ANY
+} from "crystal-event-emitter";
 import RPCClient from "ws-rpc-client";
 import { Server as WebSocketServer } from "ws";
 export class Server extends EventEmitter {
@@ -7,12 +10,19 @@ export class Server extends EventEmitter {
 			inferListeners: true
 		});
 		this.wss = new WebSocketServer(options);
-		for (let event of ["error", "headers", "connection"]) {
-			this.wss.on(event, ws => {
-				const client = new RPCClient(ws, options.rpcOptions);
-				this.emit(event, client);
-			});
+		for (let event of ["error", "headers"]) {
+			/* Forward some original events */
+			this.wss.on(event, ws => this.emit(ws));
 		}
+		this.wss.on("connection", ws => {
+			/* Forward connection events with RPCClients */
+			const client = new RPCClient(ws, options.rpcOptions);
+			this.emit(event, client);
+			/* Forward RPCClient events */
+			client.on(ANY, e => {
+				this.emit(e);
+			});
+		});
 	}
 }
 export default Server;
